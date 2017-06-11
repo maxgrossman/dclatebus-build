fccd9cf05ccd764016c088980196c18f33a54013
maxgrossman <m.grossman1813@gmail.com>
2017-06-11 14:26:45 -0400

    return route geojson
    now returning geojson of route for single direction
    will build little function to sort out if multiple directions exist / which direction exists
    and return right geojson

--- builders/make-routes.js
+++ builders/make-routes.js
@@ -2,16 +2,25 @@ var key = require('../credentials.js').apiKey;
 var rp = require('request-promise');
 var Promise = require('bluebird');
 var throttle = require('promise-ratelimit')(150);
+var turf = require('turf');
 
 module.exports = function (routeIds) {
-  return Promise.map(routeIds.slice(0, 50), (id) => {
+  return Promise.map(routeIds.slice(0, 1), (id) => {
     return throttle()
     .then(() => {
       return rp('https://api.wmata.com/Bus.svc/json/jRouteDetails?RouteID=' +
       id + '&api_key=' + key);
     })
     .then((route) => {
-      return JSON.parse(route);
+      let parsedRoute = JSON.parse(route);
+      return turf.multiPoint(
+        parsedRoute.Direction0.Shape.map((point) => {
+          return [point.Lat, point.Lon];
+        }),
+        {
+          RouteID: parsedRoute.RouteID
+        }
+      );
     })
     .catch((error) => {
       console.log(error);
