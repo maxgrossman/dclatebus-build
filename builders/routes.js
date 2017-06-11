95e6c41cadbfad1c432a53425dba9bf879120c7f
maxgrossman <m.grossman1813@gmail.com>
2017-06-11 10:13:25 -0400

    return routes
    routes.js now grabs route names from api and exports them as a thenable,
    accessable elsewhere like routes.then((route)=>//do stuff)
    will build upon currentlyr returned thenable to include the routes actual geom and
    route points geom

--- builders/routes.js
+++ builders/routes.js
@@ -1,6 +1,15 @@
 var Promise = require('bluebird');
 var rp = require('request-promise');
-var key = require('../credentials.js').apiKey
+var key = require('../credentials.js').apiKey;
 
-// Build routes list w/jRoutes & jRouteDetails end points
-// rp("https://api.wmata.com/Bus.svc/json/jRoutes")
+/* Build routes list w/jRoutes & jRouteDetails end points
+ *  1. Get list of routes
+ *  2. Pass those into a promise.map, format them as needed, export the result
+ */
+module.exports = rp('https://api.wmata.com/Bus.svc/json/jRoutes?api_key=' + key)
+.then((results) => {
+  const routes = JSON.parse(results).Routes;
+  return Object.keys(routes).map((k, v) => {
+    return routes[k].RouteID;
+  });
+});
