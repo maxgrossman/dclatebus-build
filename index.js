34f74a34b0bcdab3610f9f3967bcd58ec876c922
maxgrossman <m.grossman1813@gmail.com>
2017-06-18 18:24:43 -0400

    i screwed up
    don't tell my geography professors, but I didn't put lon and lat in the right order in my geojsons. :-{'

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 var routeIds = require('./builders/route-ids.js');
 var makeRoutes = require('./builders/route-maker.js');
+var turf = require('turf');
+var fs = require('fs');
 
 routeIds.then((ids) => {
   makeRoutes(ids).then((routes) => {
-    console.log(routes);
+    // stuff
   });
 });
